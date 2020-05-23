33f631c8d63b92ee508ed64d28870405e84fdee6
Candace <ccross@mit.edu>
2020-05-23 15:26:07 -0400

    returning object class

--- vilbert/datasets/_image_features_reader.py
+++ vilbert/datasets/_image_features_reader.py
@@ -42,7 +42,7 @@ class ImageFeaturesH5Reader(object):
         self.env = lmdb.open(self.features_path, max_readers=1, readonly=True,
                             lock=False, readahead=False, meminit=False)
 
-        with self.env.begin(write=False) as txn: 
+        with self.env.begin(write=False) as txn:
             self._image_ids = pickle.loads(txn.get('keys'.encode()))
 
         self.features = [None] * len(self._image_ids)
@@ -54,7 +54,7 @@ class ImageFeaturesH5Reader(object):
         return len(self._image_ids)
 
     def __getitem__(self, image_id):
-        image_id = str(image_id).encode()
+        image_id = str(image_id).encode()  
         index = self._image_ids.index(image_id)
         if self._in_memory:
             # Load features during first epoch, all not loaded together as it
@@ -132,6 +132,98 @@ class ImageFeaturesH5Reader(object):
                 image_location_ori = np.concatenate([np.expand_dims(g_location_ori, axis=0), image_location_ori], axis=0)
 
         return features, num_boxes, image_location, image_location_ori
+
+    def keys(self) -> List[int]:
+        return self._image_ids
+
+
+
+class ImageFeaturesH5ReaderWithObjClasses(ImageFeaturesH5Reader):
+    """ additionally returns object class
+    """
+    def __getitem__(self, image_id):
+        image_id = str(image_id).encode()  
+        index = self._image_ids.index(image_id)
+        if self._in_memory:
+            raise Exception('not yet implemented')
+            # Load features during first epoch, all not loaded together as it
+            # has a slow start.
+            if self.features[index] is not None:
+                features = self.features[index]
+                num_boxes = self.num_boxes[index]
+                image_location = self.boxes[index]
+                image_location_ori = self.boxes_ori[index]
+            else:
+                with self.env.begin(write=False) as txn:
+                    item = pickle.loads(txn.get(image_id))
+                    image_id = item['image_id']
+                    image_h = int(item['image_h'])
+                    image_w = int(item['image_w'])
+                    num_boxes = int(item['num_boxes'])
+
+                    features = np.frombuffer(base64.b64decode(item["features"]), dtype=np.float32).reshape(num_boxes, 2048)
+                    boxes = np.frombuffer(base64.b64decode(item['boxes']), dtype=np.float32).reshape(num_boxes, 4)
+                    
+                    g_feat = np.sum(features, axis=0) / num_boxes
+                    num_boxes = num_boxes + 1
+
+                    features = np.concatenate([np.expand_dims(g_feat, axis=0), features], axis=0)
+                    self.features[index] = features
+
+                    image_location = np.zeros((boxes.shape[0], 5), dtype=np.float32)
+                    image_location[:,:4] = boxes
+                    image_location[:,4] = (image_location[:,3] - image_location[:,1]) * (image_location[:,2] - image_location[:,0]) / (float(image_w) * float(image_h))
+
+                    image_location_ori = copy.deepcopy(image_location)
+
+                    image_location[:,0] = image_location[:,0] / float(image_w)
+                    image_location[:,1] = image_location[:,1] / float(image_h)
+                    image_location[:,2] = image_location[:,2] / float(image_w)
+                    image_location[:,3] = image_location[:,3] / float(image_h)
+
+                    g_location = np.array([0,0,1,1,1])
+                    image_location = np.concatenate([np.expand_dims(g_location, axis=0), image_location], axis=0)
+                    self.boxes[index] = image_location
+
+                    g_location_ori = np.array([0,0,image_w,image_h,image_w*image_h])
+                    image_location_ori = np.concatenate([np.expand_dims(g_location_ori, axis=0), image_location_ori], axis=0)
+                    self.boxes_ori[index] = image_location_ori
+                    self.num_boxes[index] = num_boxes               
+        else:
+            # Read chunk from file everytime if not loaded in memory.    
+            with self.env.begin(write=False) as txn:
+                item = pickle.loads(txn.get(image_id))
+                image_id = item['image_id']
+                image_h = int(item['image_h'])
+                image_w = int(item['image_w'])
+                num_boxes = int(item['num_boxes'])
+
+                features = np.frombuffer(base64.b64decode(item["features"]), dtype=np.float32)\
+                             .reshape(num_boxes, 2048)
+                boxes = np.frombuffer(base64.b64decode(item['boxes']), dtype=np.float32)\
+                          .reshape(num_boxes, 4)
+                classes = np.frombuffer(base64.b64decode(item['classes']), dtype=np.float32)\
+                g_feat = np.sum(features, axis=0) / num_boxes
+                num_boxes = num_boxes + 1
+                features = np.concatenate([np.expand_dims(g_feat, axis=0), features], axis=0)
+                
+                image_location = np.zeros((boxes.shape[0], 5), dtype=np.float32)
+                image_location[:,:4] = boxes
+                image_location[:,4] = (image_location[:,3] - image_location[:,1]) * (image_location[:,2] - image_location[:,0]) / (float(image_w) * float(image_h))
+
+                image_location_ori = copy.deepcopy(image_location)
+                image_location[:,0] = image_location[:,0] / float(image_w)
+                image_location[:,1] = image_location[:,1] / float(image_h)
+                image_location[:,2] = image_location[:,2] / float(image_w)
+                image_location[:,3] = image_location[:,3] / float(image_h)
+
+                g_location = np.array([0,0,1,1,1])
+                image_location = np.concatenate([np.expand_dims(g_location, axis=0), image_location], axis=0)
+
+                g_location_ori = np.array([0,0,image_w,image_h,image_w*image_h])
+                image_location_ori = np.concatenate([np.expand_dims(g_location_ori, axis=0), image_location_ori], axis=0)
+
+        return features, num_boxes, image_location, image_location_ori, classes
 
     def keys(self) -> List[int]:
         return self._image_ids
